1c0bbc4d158154387e179f5e68415bd0e46ddcf8
Ricard Ribatallada Soriano <Ricard@MacBook-Pro-de-Ricard.local>
2018-05-24 09:32:13 +0100

    port2

--- keystone.js
+++ keystone.js
@@ -11,7 +11,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 //port added
-const port = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 
 // Initialise Keystone with your project's configuration.
